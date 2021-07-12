98099f6b135ef8fcf7a68cbfcf4274c783174d16
Shital Chaudhary <shitalchau10@gmail.com>
2021-07-11 22:21:05 -0500

    pseudocode logic

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// start quiz btn will take user to their first question
+// once start quiz btn is clicked - the timer starts
+// each question has a 60 second timer.
+// If user answers the question wrong, they lose 10 seconds out of 60 second timer.
+// when all questions are answered or timer reaches 0 for any question - the game is over. 
+// when game is over - save user score.

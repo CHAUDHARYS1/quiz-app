3efc375adb153ea7954f39297016ab9a6e21f1e4
Shital Chaudhary <shitalchaudhary@Sharmilas-MacBook-Pro.local>
2021-07-12 13:37:48 -0500

    start base js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@
 // If user answers the question wrong, they lose 10 seconds out of 60 second timer.
 // when all questions are answered or timer reaches 0 for any question - the game is over. 
 // when game is over - save user score.
+
+
+var startBtn = document.querySelector("#start-quiz-btn");
+console.log(startBtn);
+
+var startTimer = function(){
+    var timerValue = document.querySelector("#timer");
+
+    console.log(startTimer);
+}

e1fb04a6d500746185e0e3788d16a294b931e87b
Shital Chaudhary <shitalchau10@gmail.com>
2021-11-05 22:34:28 -0500

    selected answers are active

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,19 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const scoreContainerElement = document.getElementById('score-container')
 
-// // start score from 0
-// var score = 0;
-// var scoreIncrement = 10;
-
-// // select 'start quiz' button 
-// var startQuizBtn = document.querySelector("#start-quiz-btn");
-// // select div containerelement
-// var mainEl = document.querySelector("#introduction");
-// //
-// var questionEl = document.querySelector(".question-container");
-
-// var questionHeading = document.querySelector("#questions");
-// var answerList = document.querySelector("#answer-list");
-
-// var displayedQuestion = 0;
+const displayScoreContainerElement = document.getElementById('display-score')
+const totalScore = document.getElementById('score')
+const nameInput = document.getElementById('name')
+let currentScore = 0;
 
  // start the countdown from 75
  var timerEl = document.querySelector("#timer");
  var seconds = 75;
- var secondsDeduction = 10;
+ var secondsDeduction = 1;
 
 
 // start timer as soon as 'Start Quiz' button is pressed
@@ -33,7 +28,7 @@ function startTimer() {
             clearInterval(cancel);
         }
         timerEl.innerHTML = `${--seconds}`;
-        if (seconds === 0) {
+        if (seconds <= 0) {
             alert("You ran out of time. Good luck next time!\n\nThe page will now redirect to the main page.");
 
             // reload the page back the main section
@@ -42,13 +37,6 @@ function startTimer() {
         }
     }, 1000);
 }
-
-
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
 
@@ -97,6 +85,11 @@ function resetState() {
 function selectAnswer(e) {
   const selectedButton = e.target
   const correct = selectedButton.dataset.correct
+  console.log(typeof [...answerButtonsElement.children]);
+  [...answerButtonsElement.children].map(btn => {
+    btn.classList.remove('btn-active');
+  })
+  selectedButton.classList.add('btn-active')
   setStatusClass(document.body, correct)
   Array.from(answerButtonsElement.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
@@ -104,6 +97,7 @@ function selectAnswer(e) {
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
   } else {
+    // 
     startButton.innerText = 'Restart'
     startButton.classList.remove('hide')
   }
@@ -113,8 +107,10 @@ function setStatusClass(element, correct) {
   clearStatusClass(element)
   if (correct) {
     element.classList.add('correct')
+    currentScore ++
   } else {
     element.classList.add('wrong')
+    seconds -= secondsDeduction
   }
 }
 
@@ -137,9 +133,9 @@ const questions = [
     question: 'The condition in an if/else statement is enclosed with ______.',
     answers: [
       { text: 'quotes', correct: true },
-      { text: 'curly brackets', correct: true },
-      { text: 'parenthesis', correct: true },
-      { text: 'square brackets', correct: true }
+      { text: 'curly brackets', correct: false },
+      { text: 'parenthesis', correct: false },
+      { text: 'square brackets', correct: false }
     ]
   },
   {
@@ -155,7 +151,7 @@ const questions = [
     question: 'String values must be enclosed within _____ when being assigned to variables.',
     answers: [
       { text: 'quotes', correct: false },
-      { text: 'commas', correct: true },
+      { text: 'commas', correct: false },
       { text: 'curly brackets', correct: false },
       { text: 'parenthesis', correct: true }
     ]
@@ -164,7 +160,7 @@ const questions = [
     question: 'A very useful tool used during development and debugging for printing content to the debugger is',
     answers: [
       { text: 'console.log', correct: false },
-      { text: 'javascript', correct: true },
+      { text: 'javascript', correct: false },
       { text: 'terminal/bash', correct: false },
       { text: 'for loops', correct: true }
     ]
